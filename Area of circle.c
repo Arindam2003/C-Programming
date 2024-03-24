45ce0f11991df967feef8995c60b79bedc630b8e
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2024-03-24 21:26:56 +0530

    Add files via upload

---
+++ Area of circle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	float a,r;
+	printf("Enter radius: ");
+	scanf("%f",&r);
+	a=(3.14*r*r);
+	printf ("Area is...%f" ,a);
+	getch();
+}
---
+++ Area of rectangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	float a,l,b;
+	printf("Enter length: ");
+	scanf("%f",&l);
+	printf("Enter breadth: ");
+	scanf("%f",&b);
+	a=l*b;
+	printf("Area of rectangle=%f",a);
+	getch();
+	
+	
+	
+}
